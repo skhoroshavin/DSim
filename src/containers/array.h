4c07982162fc998666017132dc659ce1656a2e5a
Sergey Khoroshavin <skhoroshavin@gmail.com>
2016-11-12 02:38:26 +0300

    Making array code more consistent

--- src/containers/array.h
+++ src/containers/array.h
@@ -33,24 +33,24 @@ void _dsim_array_reset( _dsim_array *a, uint32_t elem_size );
         uint32_t capacity; \
         dsim_allocator * alloc; \
     } dsim_##type##_array; \
-    inline static void dsim_##type##_array##_reserve( dsim_##type##_array *array, uint32_t count ) \
-    { _dsim_array_reserve( (_dsim_array*)array, count, sizeof(type)); } \
-    inline static void dsim_##type##_array_resize( dsim_##type##_array *array, uint32_t count ) \
-    { _dsim_array_resize( (_dsim_array*)array, count, sizeof(type) ); } \
-    inline static void dsim_##type##_array_fill( dsim_##type##_array *array, type value ) \
-    { _dsim_array_fill( (_dsim_array*)array, &value, sizeof(type) ); } \
-    inline static void dsim_##type##_array_push_back( dsim_##type##_array *array, type value ) \
-    { _dsim_array_push_back( (_dsim_array*)array, &value, 1, sizeof(type) ); } \
-    inline static void dsim_##type##_array_push_back_n( dsim_##type##_array *array, const type * data, uint32_t count ) \
-    { _dsim_array_push_back( (_dsim_array*)array, data, count, sizeof(type) ); } \
-    inline static void dsim_##type##_array_pop_back( dsim_##type##_array *array ) \
-    { assert(array->count); --array->count; } \
-    inline static void dsim_##type##_array_pop_back_n( dsim_##type##_array *array, uint32_t count ) \
-    { assert(array->count >= count ); array->count -= count; } \
-    inline static void dsim_##type##_array_clear( dsim_##type##_array *array ) \
-    { array->count = 0; } \
-    inline static void dsim_##type##_array_reset( dsim_##type##_array *array ) \
-    { _dsim_array_reset( (_dsim_array*)array, sizeof(type) ); } \
+    inline static void dsim_##type##_array##_reserve( dsim_##type##_array *a, uint32_t count ) \
+    { _dsim_array_reserve( (_dsim_array*)a, count, sizeof(type)); } \
+    inline static void dsim_##type##_array_resize( dsim_##type##_array *a, uint32_t count ) \
+    { _dsim_array_resize( (_dsim_array*)a, count, sizeof(type) ); } \
+    inline static void dsim_##type##_array_fill( dsim_##type##_array *a, type value ) \
+    { _dsim_array_fill( (_dsim_array*)a, &value, sizeof(type) ); } \
+    inline static void dsim_##type##_array_push_back( dsim_##type##_array *a, type value ) \
+    { _dsim_array_push_back( (_dsim_array*)a, &value, 1, sizeof(type) ); } \
+    inline static void dsim_##type##_array_push_back_n( dsim_##type##_array *a, const type * data, uint32_t count ) \
+    { _dsim_array_push_back( (_dsim_array*)a, data, count, sizeof(type) ); } \
+    inline static void dsim_##type##_array_pop_back( dsim_##type##_array *a ) \
+    { assert(a->count); --a->count; } \
+    inline static void dsim_##type##_array_pop_back_n( dsim_##type##_array *a, uint32_t count ) \
+    { assert(a->count >= count ); a->count -= count; } \
+    inline static void dsim_##type##_array_clear( dsim_##type##_array *a ) \
+    { a->count = 0; } \
+    inline static void dsim_##type##_array_reset( dsim_##type##_array *a ) \
+    { _dsim_array_reset( (_dsim_array*)a, sizeof(type) ); } \
 
 DSIM_DEFINE_ARRAY(uint32)
 DSIM_DEFINE_ARRAY(uint64)
